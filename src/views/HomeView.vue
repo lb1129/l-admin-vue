8d9c82c06da6aa971bfdd61bf7f48be2ba840756
leiboT <superviho1@gmail.com>
2023-05-22 20:52:23 +0800

    chore: i18n使用示例

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 import HelloWorld from '../components/HelloWorld.vue'
+const { t, locale, availableLocales } = useI18n()
 </script>
 
 <template>
   <main>
-    <HelloWorld msg="HelloWorld" />
+    <HelloWorld :msg="t('helloWorld')" />
+    <select v-model="locale">
+      <option v-for="localeStr in availableLocales" :key="localeStr" :value="localeStr">
+        {{ localeStr }}
+      </option>
+    </select>
   </main>
 </template>
