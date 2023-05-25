0f7a98788d363066e4d68bc9d9ac747866480de0
leiboT <superviho1@gmail.com>
2023-05-25 21:14:28 +0800

    ci: 新增 VITE_SYSTEM_NAME 环境变量

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SYSTEM_NAME: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
