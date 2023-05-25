dc89993aeaf3c3efae94b3f178653450aa353f15
leiboT <superviho1@gmail.com>
2023-05-25 21:18:25 +0800

    feat: ToggleLanguage组件

---
+++ src/components/ToggleLanguage.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-dropdown>
+    <span v-bind="$attrs">
+      <GlobalOutlined style="font-size: 16px" />
+    </span>
+    <template #overlay>
+      <a-menu :selected-keys="selectedKeys" @click="toggleLanguage">
+        <a-menu-item key="zh-cn">简体中文</a-menu-item>
+        <a-menu-item key="en">English</a-menu-item>
+      </a-menu>
+    </template>
+  </a-dropdown>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { GlobalOutlined } from '@ant-design/icons-vue'
+import { type MenuInfo } from 'ant-design-vue/es/menu/src/interface'
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n()
+const toggleLanguage = (menuInfo: MenuInfo) => {
+  locale.value = menuInfo.key as string
+}
+const selectedKeys = computed(() => [locale.value])
+</script>
+
+<style scoped></style>
