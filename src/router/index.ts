b1337a61be73909c3b1f8c4b5281858a0e005537
leiboT <superviho1@gmail.com>
2023-05-25 21:31:03 +0800

    chore: 路由配置拆分以及添加导航守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import routes from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      // 按需加载 () => import('../views/HomeView.vue')
-      component: HomeView
-    }
-  ]
+  routes
+})
+
+router.beforeEach(async (to, from) => {
+  // 是否登录
+  const isAuthenticated = true
+  // 是否有权限
+  const isAuthorizated = true
+  if (!isAuthenticated) {
+    // 将用户重定向到登录页面
+    return { name: 'login' }
+  }
+})
+
+router.afterEach((to, from) => {
+  const title = to.meta.title
+  const prefix = title ? `${title} - ` : ''
+  document.title = `${prefix}${import.meta.env.VITE_SYSTEM_NAME}`
 })
 
 export default router
