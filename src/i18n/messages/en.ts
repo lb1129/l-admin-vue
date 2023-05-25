49533265cffa8d16c71ceff5dadc84dcac73323d
leiboT <superviho1@gmail.com>
2023-05-25 21:34:26 +0800

    chore: 更新多语言资源

--- src/i18n/messages/en.ts
+++ src/i18n/messages/en.ts
@@ -1,3 +1,27 @@
 export default {
-  helloWorld: 'HelloWorld'
+  helloWorld: 'Hello World',
+  privacyPolicy: 'Privacy policy',
+  signUp: 'Sign up',
+  forgotPassword: 'Forgot password?',
+  login: 'Login',
+  password: 'Password',
+  account: 'Account',
+  pleaseEnterAccount: 'Please enter account',
+  pleaseEnterPassword: 'Please enter password',
+  pleaseEnterNewPassword: 'Please enter new password',
+  pleaseEnterMobileNumber: 'Please enter mobile number',
+  pleaseEnterVerificationCode: 'Please enter verification code',
+  welcome: 'Welcome',
+  confirmPassword: 'Confirm password',
+  mobileNumber: 'Mobile number',
+  verificationCode: 'Verification code',
+  getVerificationCode: 'Get code',
+  haveAnAccount: 'Already have an account?',
+  register: 'Register',
+  newPassword: 'New password',
+  twoPasswordsDoNotMatch: 'The passwords entered twice do not match',
+  confirm: 'Confirm',
+  back: 'Back',
+  pageNotFound: 'Page not found',
+  systemDesc: 'This is a description of {0}'
 }
--- src/i18n/messages/zh-cn.ts
+++ src/i18n/messages/zh-cn.ts
@@ -1,3 +1,27 @@
 export default {
-  helloWorld: '你好世界'
+  helloWorld: '你好世界',
+  privacyPolicy: '隐私政策',
+  signUp: '注册账号',
+  forgotPassword: '忘记密码',
+  login: '登录',
+  password: '密码',
+  account: '用户名',
+  pleaseEnterAccount: '请输入用户名',
+  pleaseEnterPassword: '请输入密码',
+  pleaseEnterNewPassword: '请输入新密码',
+  pleaseEnterMobileNumber: '请输入手机号',
+  pleaseEnterVerificationCode: '请输入验证码',
+  welcome: '欢迎回来',
+  confirmPassword: '确认密码',
+  mobileNumber: '手机号',
+  verificationCode: '验证码',
+  getVerificationCode: '获取验证码',
+  haveAnAccount: '使用已有账号登录',
+  register: '注册',
+  newPassword: '新密码',
+  twoPasswordsDoNotMatch: '两次输入的密码不匹配',
+  confirm: '确认',
+  back: '返回',
+  pageNotFound: '页面不存在',
+  systemDesc: '这是对 {0} 的描述'
 }
