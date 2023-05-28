91923deb28e76b1d77b88e33276ddd43921af077
leiboT <superviho1@gmail.com>
2023-05-28 16:13:33 +0800

    chore: 个人中心页面

---
+++ src/views/user-management/PersonalCenter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="personal-center">个人中心</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.personal-center {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+}
+</style>
