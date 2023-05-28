a49e88930a4af9814af623adf94d38db7831b412
leiboT <superviho1@gmail.com>
2023-05-28 16:12:19 +0800

    chore: 首页

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">首页</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.home {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+}
+</style>
