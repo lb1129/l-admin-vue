28b8f72cd1c253116c0e08e4be3c9f04cf5dbd95
leiboT <superviho1@gmail.com>
2023-05-30 00:47:53 +0800

    chore: 配置antd多语言

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { watchEffect, ref } from 'vue'
+import enUS from 'ant-design-vue/es/locale/en_US'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/es'
+import 'dayjs/locale/zh-cn'
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n()
+const aLocale = ref<typeof enUS>()
+watchEffect(() => {
+  if (locale.value === 'en') {
+    aLocale.value = enUS
+    dayjs.locale('en')
+  } else {
+    aLocale.value = zhCN
+    dayjs.locale('zh-cn')
+  }
+})
+</script>
 
 <template>
-  <router-view />
+  <a-config-provider :locale="aLocale">
+    <router-view />
+  </a-config-provider>
 </template>
 
 <style scoped></style>
