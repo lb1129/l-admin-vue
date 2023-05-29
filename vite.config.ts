726f84ab88ca4883fa4f0c2a483ef7e7aa42be75
leiboT <superviho1@gmail.com>
2023-05-30 03:58:35 +0800

    ci: 配置node_modules别名

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./node_modules', import.meta.url))
     }
   },
   css: {
