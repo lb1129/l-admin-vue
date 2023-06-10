dff8d0c2023e27d323632ad8564e2e8c21935a30
leiboT <superviho1@gmail.com>
2023-06-10 22:41:02 +0800

    chore: echarts使用示例调整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,232 +11,58 @@
 import { ref, onMounted } from 'vue'
 import { type PositionInfo } from '@/components/PositionMap.vue'
 import * as echarts from 'echarts/core'
-import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
-import { PieChart } from 'echarts/charts'
-import { LabelLayout } from 'echarts/features'
+import { GridComponent, TooltipComponent } from 'echarts/components'
+import { BarChart } from 'echarts/charts'
 import { CanvasRenderer } from 'echarts/renderers'
+// echarts 按需加载使用
+echarts.use([GridComponent, TooltipComponent, BarChart, CanvasRenderer])
 
-echarts.use([
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-  PieChart,
-  CanvasRenderer,
-  LabelLayout
-])
 const chartNode = ref<HTMLElement>()
 onMounted(() => {
   // 绘制图表
   const chartNodeRaw = chartNode.value
-  const genData = (count: number) => {
-    const nameList = [
-      '赵',
-      '钱',
-      '孙',
-      '李',
-      '周',
-      '吴',
-      '郑',
-      '王',
-      '冯',
-      '陈',
-      '褚',
-      '卫',
-      '蒋',
-      '沈',
-      '韩',
-      '杨',
-      '朱',
-      '秦',
-      '尤',
-      '许',
-      '何',
-      '吕',
-      '施',
-      '张',
-      '孔',
-      '曹',
-      '严',
-      '华',
-      '金',
-      '魏',
-      '陶',
-      '姜',
-      '戚',
-      '谢',
-      '邹',
-      '喻',
-      '柏',
-      '水',
-      '窦',
-      '章',
-      '云',
-      '苏',
-      '潘',
-      '葛',
-      '奚',
-      '范',
-      '彭',
-      '郎',
-      '鲁',
-      '韦',
-      '昌',
-      '马',
-      '苗',
-      '凤',
-      '花',
-      '方',
-      '俞',
-      '任',
-      '袁',
-      '柳',
-      '酆',
-      '鲍',
-      '史',
-      '唐',
-      '费',
-      '廉',
-      '岑',
-      '薛',
-      '雷',
-      '贺',
-      '倪',
-      '汤',
-      '滕',
-      '殷',
-      '罗',
-      '毕',
-      '郝',
-      '邬',
-      '安',
-      '常',
-      '乐',
-      '于',
-      '时',
-      '傅',
-      '皮',
-      '卞',
-      '齐',
-      '康',
-      '伍',
-      '余',
-      '元',
-      '卜',
-      '顾',
-      '孟',
-      '平',
-      '黄',
-      '和',
-      '穆',
-      '萧',
-      '尹',
-      '姚',
-      '邵',
-      '湛',
-      '汪',
-      '祁',
-      '毛',
-      '禹',
-      '狄',
-      '米',
-      '贝',
-      '明',
-      '臧',
-      '计',
-      '伏',
-      '成',
-      '戴',
-      '谈',
-      '宋',
-      '茅',
-      '庞',
-      '熊',
-      '纪',
-      '舒',
-      '屈',
-      '项',
-      '祝',
-      '董',
-      '梁',
-      '杜',
-      '阮',
-      '蓝',
-      '闵',
-      '席',
-      '季',
-      '麻',
-      '强',
-      '贾',
-      '路',
-      '娄',
-      '危'
-    ]
-    const legendData = []
-    const seriesData = []
-    for (var i = 0; i < count; i++) {
-      var name = Math.random() > 0.65 ? makeWord(4, 1) + '·' + makeWord(3, 0) : makeWord(2, 1)
-      legendData.push(name)
-      seriesData.push({
-        name: name,
-        value: Math.round(Math.random() * 100000)
-      })
-    }
-    return {
-      legendData: legendData,
-      seriesData: seriesData
-    }
-    function makeWord(max: number, min: number) {
-      const nameLen = Math.ceil(Math.random() * max + min)
-      const name = []
-      for (var i = 0; i < nameLen; i++) {
-        name.push(nameList[Math.round(Math.random() * nameList.length - 1)])
-      }
-      return name.join('')
-    }
-  }
   if (chartNodeRaw) {
     var myChart = echarts.init(chartNodeRaw)
-    var data = genData(50)
     // 绘制图表
     myChart.setOption({
-      title: {
-        text: '同名数量统计',
-        subtext: '纯属虚构'
-      },
       tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
       },
-      legend: {
-        type: 'scroll',
-        orient: 'vertical',
-        right: 10,
-        top: 20,
-        bottom: 20,
-        data: data.legendData
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
       },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
       series: [
         {
-          name: '姓名',
-          type: 'pie',
-          radius: '55%',
-          center: ['40%', '50%'],
-          data: data.seriesData,
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
+          name: 'Direct',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
         }
       ]
     })
   }
 })
-const changeHandler = (data: PositionInfo) => {
-  console.log(data)
-}
+const changeHandler = (data: PositionInfo) => {}
 </script>
 
 <style scoped lang="less">
