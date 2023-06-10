da7673ddebb52fca7ba82e1b0c42f308fd5049d3
leiboT <superviho1@gmail.com>
2023-06-10 22:43:31 +0800

    chore: 布局样式调整

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,86 +1,70 @@
 <template>
   <a-layout class="index">
-    <a-layout-sider breakpoint="lg" v-model:collapsed="collapsed" :trigger="null" collapsible>
-      <div class="index-slider">
-        <div class="index-slider-logo" />
-        <div class="index-slider-content">
-          <vuescroll
-            :ops="{
-              bar: {
-                background: '#fff',
-                opacity: 0.5
-              }
-            }"
-          >
-            <a-menu
-              @click="leftMenuItemClickHandle"
-              :selectedKeys="selectedKeys"
-              v-model:openKeys="openKeys"
-              theme="dark"
-              mode="inline"
-            >
-              <a-menu-item key="Home">
-                <dashboard-outlined />
-                <span>首页</span>
-              </a-menu-item>
-              <a-sub-menu key="ProductManagement">
-                <template #title>
-                  <span>
-                    <shop-outlined />
-                    <span>产品管理</span>
-                  </span>
-                </template>
-                <a-menu-item key="ProductList">
-                  <span>产品列表</span>
-                </a-menu-item>
-              </a-sub-menu>
-              <a-sub-menu key="UserManagement">
-                <template #title>
-                  <span>
-                    <user-outlined />
-                    <span>用户管理</span>
-                  </span>
-                </template>
-                <a-menu-item key="PersonalCenter">
-                  <span>个人中心</span>
-                </a-menu-item>
-              </a-sub-menu>
-            </a-menu>
-          </vuescroll>
-        </div>
+    <a-layout-header class="index-header">
+      <div class="index-header-logo">
+        <a href="/">
+          <img :src="logoSvg" alt="logo" />
+          {{ systemName }}
+        </a>
       </div>
-    </a-layout-sider>
+      <div class="index-header-center"></div>
+      <div class="index-header-right">
+        <a-dropdown>
+          <span class="index-header-right-item index-header-right-item_user">
+            <a-avatar size="small" :src="userPng" />
+            <span style="margin-left: 8px">viho</span>
+          </span>
+          <template #overlay>
+            <a-menu @click="topRightMenuItemClickHandle">
+              <a-menu-item key="PersonalCenter">个人中心</a-menu-item>
+              <a-menu-divider />
+              <a-menu-item key="logout">退出登录</a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+        <ToggleLanguage class="index-header-right-item" />
+      </div>
+    </a-layout-header>
     <a-layout>
-      <a-layout-header class="index-header">
-        <menu-unfold-outlined
-          v-if="collapsed"
-          class="index-header-trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-        <menu-fold-outlined
-          v-else
-          class="index-header-trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-        <div class="index-header-right">
-          <a-dropdown>
-            <span class="index-header-right-item">
-              <user-outlined style="font-size: 16px" />
-              xxxx
-            </span>
-            <template #overlay>
-              <a-menu @click="topRightMenuItemClickHandle">
-                <a-menu-item key="PersonalCenter">个人中心</a-menu-item>
-                <a-menu-divider />
-                <a-menu-item key="logout">退出登录</a-menu-item>
-              </a-menu>
-            </template>
-          </a-dropdown>
-          <ToggleLanguage class="index-header-right-item" />
+      <a-layout-sider collapsible :collapsedWidth="48" :width="208" theme="light">
+        <div class="index-slider-content">
+          <a-menu
+            @click="leftMenuItemClickHandle"
+            :selectedKeys="selectedKeys"
+            v-model:openKeys="openKeys"
+            mode="inline"
+          >
+            <a-menu-item key="Home">
+              <dashboard-outlined />
+              <span>首页</span>
+            </a-menu-item>
+            <a-sub-menu key="ProductManagement">
+              <template #title>
+                <span>
+                  <shop-outlined />
+                  <span>产品管理</span>
+                </span>
+              </template>
+              <a-menu-item key="ProductList">
+                <span>产品列表</span>
+              </a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="UserManagement">
+              <template #title>
+                <span>
+                  <user-outlined />
+                  <span>用户管理</span>
+                </span>
+              </template>
+              <a-menu-item key="PersonalCenter">
+                <span>个人中心</span>
+              </a-menu-item>
+            </a-sub-menu>
+          </a-menu>
         </div>
-      </a-layout-header>
-      <a-layout-content class="index-content">
-        <a-breadcrumb class="index-content-breadcrumb">
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="(item.name as string)">
             <a
               @click="
@@ -92,7 +76,7 @@
             >
           </a-breadcrumb-item>
         </a-breadcrumb>
-        <div class="index-content-box">
+        <a-layout-content class="index-content">
           <router-view v-slot="{ Component }">
             <transition
               mode="out-in"
@@ -109,8 +93,8 @@
               </keep-alive>
             </transition>
           </router-view>
-        </div>
-      </a-layout-content>
+        </a-layout-content>
+      </a-layout>
     </a-layout>
   </a-layout>
 </template>
@@ -118,18 +102,19 @@
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue'
 import { storeToRefs } from 'pinia'
-import vuescroll from 'vuescroll'
 import { type MenuInfo } from 'ant-design-vue/es/menu/src/interface'
 import { useRoute, useRouter } from 'vue-router'
 import { useRouteOperateState, RouteOperateState } from '@/pinia/stores/route-operate-state'
 import { useBreadcrumbList } from '@/pinia/stores/breadcrumb-list'
 import { Modal } from 'ant-design-vue'
 import 'ant-design-vue/es/modal/style/index.css'
+import logoSvg from '@/assets/image/logo.svg'
+import userPng from '@/assets/image/user.png'
+const systemName = import.meta.env.VITE_SYSTEM_NAME
 const { routeOperateState } = storeToRefs(useRouteOperateState())
 const { breadcrumbList } = storeToRefs(useBreadcrumbList())
 const route = useRoute()
 const router = useRouter()
-const collapsed = ref<boolean>(false)
 // 左侧菜单选中keys
 const selectedKeys = computed(() => [route.name as string])
 // 左侧菜单打开keys
@@ -187,72 +172,61 @@ const topRightMenuItemClickHandle = (menuInfo: MenuInfo) => {
 .index {
   height: 100%;
 
-  &-slider {
-    height: 100%;
+  &-header {
     display: flex;
-    flex-direction: column;
+    align-items: center;
+    padding: 0 16px;
+    color: @white;
 
     &-logo {
-      flex: 0 0 30px;
-      margin: 16px;
-      background: rgb(255 255 255 / 30%);
-    }
-
-    &-content {
-      flex: auto;
-      min-height: 0;
-      overflow: auto;
-    }
-  }
-
-  &-header {
-    padding: 0 16px;
-    background-color: #fff;
-
-    :deep(.anticon) {
-      color: rgb(0 0 0 / 65%);
-    }
-
-    &-trigger {
+      font-weight: 600;
       font-size: 18px;
-      cursor: pointer;
-      transition: color 0.3s;
 
-      &:hover {
-        color: #1890ff;
+      & > a {
+        display: flex;
+        align-items: center;
+        color: @white;
+
+        & > img {
+          width: 38px;
+          height: 38px;
+        }
       }
+    }
+
+    &-center {
+      flex: 1;
     }
 
     &-right {
-      float: right;
-    }
+      display: flex;
 
-    &-right :deep(.index-header-right-item) {
-      display: inline-block;
-      padding: 0 12px;
-      cursor: pointer;
+      :deep(&-item) {
+        padding: 0 12px;
+        cursor: pointer;
+        transition: all 0.3s;
 
-      &:hover {
-        background-color: rgb(0 0 0 / 2.5%);
+        &:hover {
+          background-color: #252a3d;
+        }
+      }
+
+      &-item_user {
+        display: flex;
+        align-items: center;
       }
     }
   }
 
+  &-slider-content {
+    height: 100%;
+    overflow: auto;
+    border-bottom: @border-width-base solid @border-color-base;
+  }
+
   &-content {
-    display: flex;
-    flex-direction: column;
-    padding: 16px;
-
-    &-breadcrumb {
-      margin: 0 0 16px;
-    }
-
-    &-box {
-      flex: auto;
-      min-height: 0;
-      background-color: #fff;
-      overflow: hidden;
-    }
+    background-color: @component-background;
+    overflow: hidden;
   }
 }
 </style>
