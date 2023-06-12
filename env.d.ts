66e95de9113076fcdb6f9e63b54cb696f339d270
leiboT <superviho1@gmail.com>
2023-06-13 01:31:25 +0800

    chore: 增加 是否支持history路由模式，公共基础路径 环境变量类型注解

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_SYSTEM_NAME: string
   readonly VITE_AK: string
+  readonly VITE_NOT_SUPPORT_HISTORY: string
+  readonly VITE_PUBLIC_URL: string
   // 更多环境变量...
 }
 
