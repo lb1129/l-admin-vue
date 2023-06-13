de4cb0b2d5bc6e32bb533d45746adf49712cb706
leiboT <superviho1@gmail.com>
2023-06-14 02:40:00 +0800

    chore: breadcrumb store

---
+++ src/pinia/stores/breadcrumb.ts
@@ -0,0 +1,17 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { breadcrumbSeesion } from '@/utils/session-storage'
+
+export interface Breadcrumb {
+  routeName: string
+  menuName: string
+}
+
+export const useBreadcrumb = defineStore('breadcrumb', () => {
+  const breadcrumb = ref<Breadcrumb[]>([])
+  function setBreadcrumb(data: Breadcrumb[]) {
+    breadcrumb.value = data
+    breadcrumbSeesion.set(breadcrumb.value)
+  }
+  return { breadcrumb, setBreadcrumb }
+})
