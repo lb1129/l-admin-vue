b7188a6146254a3e8eadaf8e45947931d49eb61a
leiboT <superviho1@gmail.com>
2023-06-22 19:52:47 +0800

    chore: 重写push,路由操作状态及面包屑生成逻辑优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+  type RouteLocationRaw,
+  type RouteLocationNamedRaw
+} from 'vue-router'
 import routes from './routes'
 import { useBreadcrumb } from '@/pinia/stores/breadcrumb'
 import { useRouteOperateState, RouteOperateState } from '@/pinia/stores/routeOperateState'
-import { breadcrumbSeesion } from '@/storage/session-storage'
 import isAuthenticated from './isAuthenticated'
 
 const createHistory =
@@ -13,38 +18,40 @@ const router = createRouter({
   routes
 })
 
-let isFisrtEnter = true
 router.beforeEach((to, from) => {
-  if (to.matched[0].name === 'Index') {
-    const { breadcrumb, setBreadcrumb } = useBreadcrumb()
-    const { setRouteOperateState } = useRouteOperateState()
-    const record = { routeName: to.name as string, menuName: to.meta.menuName as string }
-    // 首次进入 初始化面包屑
-    if (isFisrtEnter) {
-      isFisrtEnter = false
-      let initBreadcrumb = breadcrumbSeesion.get()
-      if (!initBreadcrumb.length) initBreadcrumb = [record]
-      setBreadcrumb(initBreadcrumb)
-    } else {
-      // 产生面包屑数据
-      const index = breadcrumb.findIndex((item) => item.routeName === to.name)
-      if (index > -1) {
-        setRouteOperateState(RouteOperateState.back)
-        setBreadcrumb(breadcrumb.slice(0, index + 1))
-      } else {
-        if (to.meta.hidden) {
-          setRouteOperateState(RouteOperateState.forward)
-          setBreadcrumb([...breadcrumb, record])
-        } else {
-          setRouteOperateState(RouteOperateState.replace)
-          setBreadcrumb([record])
-        }
-      }
-    }
+  const { setRouteOperateState } = useRouteOperateState()
+  const { setBreadcrumb } = useBreadcrumb()
+  const toHistory = to.query.history as string
+  const fromHistory = from.query.history as string
+  // 路由操作状态
+  if (toHistory && toHistory.split(',').includes(from.name as string)) {
+    setRouteOperateState(RouteOperateState.forward)
+  } else if (fromHistory && fromHistory.split(',').includes(to.name as string)) {
+    setRouteOperateState(RouteOperateState.back)
+  } else {
+    setRouteOperateState(RouteOperateState.replace)
   }
+  // 面包屑
+  let breadcrumb = to.meta.menuName
+    ? [{ routeName: to.name as string, menuName: to.meta.menuName as string }]
+    : []
+  if (toHistory) {
+    breadcrumb = toHistory
+      .split(',')
+      .map((history) => {
+        const route = router.getRoutes().find((route) => route.name === history)
+        return {
+          routeName: history,
+          menuName: route?.meta.menuName as string
+        }
+      })
+      .concat(breadcrumb)
+  }
+  setBreadcrumb(breadcrumb)
 })
 
 router.beforeEach(async (to, from) => {
+  // 是否已登录 跳转控制
   try {
     await isAuthenticated.value
     if (to.meta.needAuth === false) return { name: 'Home' }
@@ -54,5 +61,25 @@ router.beforeEach(async (to, from) => {
 })
 
 router.afterEach((to, from) => {})
+
+// 重写push
+const { push } = router
+router.push = (to: RouteLocationRaw) => {
+  const locationNamedRaw = to as RouteLocationNamedRaw
+  const name = locationNamedRaw.name
+  // 命名路由 都应该用路由名称跳转
+  if (name) {
+    const routes = router.getRoutes()
+    const route = routes.find((route) => route.name === locationNamedRaw.name)
+    const currentRoute = router.currentRoute.value
+    if (route?.meta.hidden) {
+      if (!locationNamedRaw.query) locationNamedRaw.query = {}
+      locationNamedRaw.query.history = currentRoute.query.history
+        ? `${currentRoute.query.history},${currentRoute.name as string}`
+        : (currentRoute.name as string)
+    }
+  }
+  return push(to)
+}
 
 export default router
