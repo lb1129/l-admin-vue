a3e12a4c726fb4f26b876877d70ed2ede26141b9
leiboT <superviho1@gmail.com>
2023-07-31 01:00:14 +0800

    feat: unhandledrejection 处理

--- src/error-handler/index.ts
+++ src/error-handler/index.ts
@@ -1,5 +1,12 @@
 import { type ComponentPublicInstance } from 'vue'
 
+window.addEventListener('unhandledrejection', (e) => {
+  if (import.meta.env.MODE === 'production') {
+    e.preventDefault()
+    // TODO 处理错误，例如：报告给一个服务
+  }
+})
+
 export default (err: unknown, instance: ComponentPublicInstance | null, info: string) => {
   // TODO 处理错误，例如：报告给一个服务
   console.table(
