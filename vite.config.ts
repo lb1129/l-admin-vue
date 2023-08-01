6ef0d4dde710af96744f2a67391e80224da53ea9
leiboT <superviho1@gmail.com>
2023-08-01 20:55:58 +0800

    perf: 强制预构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig, loadEnv, type PluginOption } from 'vite'
 import viteBaseConfig from './vite.config.base'
+import fs from 'node:fs'
 
 // NOTE mergeConfig 对于 defineConfig(() => ({})) 合并无效
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const plugins: PluginOption[] = []
-  // if (mode === 'development') {
-  // }
+  // 开发环境强制预构建 ant-design-vue 所有组件样式 避免懒加载时造成页面刷新
+  const optimizeDepsAntdIncludes: string[] = []
+  if (mode === 'development') {
+    fs.readdirSync('node_modules/ant-design-vue/es').map((componentName) => {
+      if (fs.existsSync(`node_modules/ant-design-vue/es/${componentName}/style/index.js`))
+        optimizeDepsAntdIncludes.push(`ant-design-vue/es/${componentName}/style`)
+    })
+  }
   return {
     base: env.VITE_PUBLIC_URL,
     ...viteBaseConfig,
-    plugins: [...(viteBaseConfig.plugins ?? []), ...plugins]
+    plugins: [...(viteBaseConfig.plugins ?? []), ...plugins],
+    optimizeDeps: {
+      include: optimizeDepsAntdIncludes
+    }
   }
 })
